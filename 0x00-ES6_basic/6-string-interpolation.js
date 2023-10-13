8f017423048ab81b05af588487842a5fea8c3d1e
234761 <dag234761@gmail.com>
2023-10-13 11:15:15 +0000

    6th commit

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,10 @@
+export default function getSanFranciscoDescription() {
+	const year = 2017;
+	const buget = {
+		income: '$119,868',
+		gdp: '$154.2 billion',
+		capita: '$178,479',
+	};
+
+	return'As of ${year}, it was the seventh-highest income county in the united states, with a per capita personal in come of $(buget.income). As of 2015, san Franciso proper had a GDP of ${budget.gdp}, and a GDP per capital of ${buget.capita}.';
+}
